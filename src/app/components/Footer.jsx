058d37374635cb1323a8abe260f1021cd00e251c
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-08 23:07:17 -0500

    created a simple footer component and styled with tailwind

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="h-14 bg-gradient-to-r from-sky-500 to-indigo-500">
+      <p className="text-lg font-semibold text-slate-50 text-center p-3">
+        &copy;2024 Community Marketplace
+      </p>
+    </div>
+  );
+}
