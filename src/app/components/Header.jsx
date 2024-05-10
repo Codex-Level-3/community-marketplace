618a1920ec6123f8a14a7a1570ba19aefa477c2c
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-10 17:57:42 -0500

    Fixed the header + linked signin and signup pages. Also imported SignOut

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import SignOut from "./SignOut";
 
 export default function Header() {
   // handle function
@@ -72,24 +73,19 @@ export default function Header() {
               </Link>
             </li>
             <li className="max-lg:border-b max-lg:py-3 px-3">
-              <Link
-                href="/blog"
-                className="hover:text-[#007bff] text-[#333] block font-semibold text-[15px]"
-              >
-                Blog
-              </Link>
+              <SignOut />
             </li>
           </ul>
         </div>
 
         <div className="flex items-center ml-auto space-x-6">
           <button className="font-semibold text-[15px] border-none outline-none">
-            <a href="javascript:void(0)" class="text-[#007bff] hover:underline">
+            <Link href="/signin" class="text-[#007bff] hover:underline">
               Login
-            </a>
+            </Link>
           </button>
           <button className="px-4 py-2 text-sm rounded-sm font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
-            Sign up
+            <Link href="/signup">Sign up</Link>
           </button>
 
           <button onClick={handleToggle} id="toggleOpen" class="lg:hidden">
