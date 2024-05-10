66ebf48577823e387d4b90fd08e3fe915b33da53
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-10 17:44:30 -0500

    Mocked a static data source that contains fake product information.

---
+++ src/app/dashboard/components/productList.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    name: "Vintage Table",
+    description: "Great addition to any room.",
+    price: "$99.99",
+    contact: "Contact seller at example@example.com",
+    src: "table",
+  },
+  {
+    name: "Handcrafted Painting",
+    description: "Handmade with love.",
+    price: "$199.99",
+    contact: "Call seller at 123-456-7890",
+    src: "painting",
+  },
+  {
+    name: "Unique Handbag",
+    description: "One of a kind piece.",
+    price: "$49.99",
+    contact: "Message seller on Facebook",
+    src: "handbag",
+  },
+  {
+    name: "iPad Pro",
+    description: "Perfect gift for your loved ones.",
+    price: "$149.99",
+    contact: "DM seller on Instagram",
+    src: "iPad",
+  },
+  {
+    name: "Custom Jewelry",
+    description: "Adds a touch of elegance to your outfit.",
+    price: "$79.99",
+    contact: "Email seller at contact@example.com",
+    src: "jewelry",
+  },
+  {
+    name: "Artisanal Pottery",
+    description: "High-quality materials used.",
+    price: "$39.99",
+    contact: "Leave a comment for seller",
+    src: "pottery",
+  },
+];
