a0d5eb8ac0c075093885a12066774a9a75139fca
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-10 17:49:09 -0500

    Created Posts component to fetch + display posts, and manage user authentication

---
+++ src/app/dashboard/components/Posts.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { auth, db } from "@/app/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import Post from "./Post";
+
+export default function Posts() {
+  // state management
+  const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState(null);
+  const [checked, setChecked] = useState(false);
+
+  //side effects
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
+
+  useEffect(() => {
+    const userId = auth.currentUser.uid;
+
+    if (user) {
+      const showMyPost = checked ? where("userId", "==", userId) : "";
+      const q = query(collection(db, "posts"));
+      const unsub = onSnapshot(q, (snapshot) => {
+        const postArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPosts(postArray);
+      });
+      return () => unsub();
+    }
+  }, [user]);
+
+  return (
+    <section>
+      {posts.map((post) => (
+        <Post key={post.id} user={user} post={post} />
+      ))}
+    </section>
+  );
+}
