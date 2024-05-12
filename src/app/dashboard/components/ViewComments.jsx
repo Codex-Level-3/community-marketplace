f9644120c86f0a827a86976f139aaea2ded4e7d6
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-12 12:41:53 -0500

    Applied Tailwind CSS styling

--- src/app/dashboard/components/ViewComments.jsx
+++ src/app/dashboard/components/ViewComments.jsx
@@ -1,10 +1,13 @@
 export default function ViewComments({ post }) {
   return (
-    <div>
+    <div className="w-full lg:w-2/3 mx-auto">
       {post.comments.map((comment, index) => (
-        <div key={index}>
-          <p>{comment.displayName}</p>
-          <p>{comment.text}</p>
+        <div
+          key={index}
+          className="bg-white border-t border-gray-200 px-4 py-3 first:pt-0"
+        >
+          <p className="font-semibold">{comment.displayName}</p>
+          <p className="text-gray-600 text-md">{comment.text}</p>
         </div>
       ))}
     </div>
