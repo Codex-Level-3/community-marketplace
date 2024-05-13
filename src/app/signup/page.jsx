69ffd2b211f19987d533910f5a726b8db9cc4f1d
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-13 18:04:20 -0500

    changed function name

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,7 +1,7 @@
 import GoogleSignIn from "../components/GoogleSignIn";
 import Signup from "../components/Signup";
 
-export default function Page() {
+export default function page() {
   return (
     <main className="flex h-full w-full">
       <div className="h-screen w-1/2 bg-[url('https://images.unsplash.com/photo-1541976844346-f18aeac57b06?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center ">
